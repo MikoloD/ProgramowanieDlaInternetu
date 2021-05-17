817f9f5030ee84e9cf482b518e76be86b2bff083
MikoloD <grygiel.mikolaj@gmail.com>
2021-05-17 17:36:29 +0200

    change to .net 5.0

---
+++ RpgAplication/RpgApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RpgApplication.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(RpgApplication.Areas.Identity.IdentityHostingStartup))]
+namespace RpgApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DatabaseContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DatabaseContextConnection")));
+
+                services.AddDefaultIdentity<UserModel>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DatabaseContext>();
+            });
+        }
+    }
+}
